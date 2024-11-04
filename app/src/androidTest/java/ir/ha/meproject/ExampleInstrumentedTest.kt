6e27dae0e2809d88ada747ea4b8dea9f13e51fc8
Hsn <ihasan.azimi@gamil.com>
2024-11-04 14:57:08 +0330

    temp commit

--- app/src/androidTest/java/ir/ha/meproject/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ir/ha/meproject/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ir.asadiara.meproject", appContext.packageName)
+        assertEquals("ir.ha.meproject", appContext.packageName)
     }
 }
